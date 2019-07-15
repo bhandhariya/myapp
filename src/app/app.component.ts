46ac1c1391e3432139de800ac3b3c349ad37b7da
bhandhariya <sainiraja59@gmail.com>
2019-07-15 23:36:13 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myapp';
+  constructor(private http:HttpClient){}
+  run(){
+    this.http.get('/user').subscribe(r=>{
+      console.log(r)
+    })
+  }
 }
