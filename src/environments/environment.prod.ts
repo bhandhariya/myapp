07683e7be190f9be1a83b197738af2ed435070df
bhandhariya <sainiraja59@gmail.com>
2019-08-10 13:20:47 -0700

    firebase environement changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyAdjIvlNpci2vqE76yTh2TesC-sM4vUsZM",
+    authDomain: "myapp-345d5.firebaseapp.com",
+    databaseURL: "https://myapp-345d5.firebaseio.com",
+    projectId: "myapp-345d5",
+    storageBucket: "gs://myapp-345d5.appspot.com",
+    messagingSenderId: "858074289543",
+    appId: "1:858074289543:web:5b69509428b6ba8f"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,16 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebaseConfig : {
-    apiKey: "AIzaSyAdjIvlNpci2vqE76yTh2TesC-sM4vUsZM",
-    authDomain: "myapp-345d5.firebaseapp.com",
-    databaseURL: "https://myapp-345d5.firebaseio.com",
-    projectId: "myapp-345d5",
-    storageBucket: "gs://myapp-345d5.appspot.com",
-    messagingSenderId: "858074289543",
-    appId: "1:858074289543:web:5b69509428b6ba8f"
-  }
+  production: false
 };
 
 /*
